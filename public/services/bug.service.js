7e960838a7d4a39b9ff40d295a9b794882c1c494
Refael Levi <refael14levi@gmail.com>
2024-10-21 22:08:28 +0300

    cleaning

--- public/services/bug.service.js
+++ public/services/bug.service.js
@@ -17,26 +17,6 @@ export const bugService = {
     getDefaultFilter,
 }
 
-
-// function query() {
-//     return storageService.query(STORAGE_KEY)
-// }
-// function getById(bugId) {
-//     return storageService.get(STORAGE_KEY, bugId)
-// }
-
-// function remove(bugId) {
-//     return storageService.remove(STORAGE_KEY, bugId)
-// }
-
-// function save(bug) {
-//     if (bug._id) {
-//         return storageService.put(STORAGE_KEY, bug)
-//     } else {
-//         return storageService.post(STORAGE_KEY, bug)
-//     }
-// }
-
 //* sort later
 function query(filterBy,sortBy) {
     const queryParams = { ...filterBy,...sortBy }
